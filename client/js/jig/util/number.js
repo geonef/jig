ed06edae5c388b96628d38085dbf2c8e70c3e43a
okapi <okapi@a80fe19f-c1ec-4b56-a503-cf2ee58ae7cd>
2011-02-02 20:31:37 +0000

    aire
    git-svn-id: http://code.lapatate.org/svn/trunk/pkg/zig@1106 a80fe19f-c1ec-4b56-a503-cf2ee58ae7cd

--- client/js/jig/util/number.js
+++ client/js/jig/util/number.js
@@ -21,33 +21,52 @@ dojo.mixin(jig.util.number,
     return str;
   }*/
 
+  /**
+   * Format a number
+   *
+   * Forwarded to dojo.number.format, but before, the number is
+   * rounded depending on the "digits" option.
+   */
+  format: function(value, options) {
+    if (options && options.digits) {
+      var nb = Math.ceil(Math.log(value) / Math.LN10);
+      if (nb < options.digits) {
+        var factor = Math.pow(10, options.digits - nb);
+        console.log('value', value, factor, arguments);
+        value = Math.round(value * factor) / factor;
+        console.log('value after', value);
+      }
+    }
+    return dojo.number.format(value, options);
+  },
+
+
+  /**
+   * Format an array of numbers (called "dimensions")
+   *
+   * Can typically work on 2D or 3D coordinates, sizes...
+   * Can also work on simple numbers (call: formatDims([number]))
+   *
+   * Examples:
+   *      formatDims([fileWeight], { mult: 1024,
+   *                                 units: ['B', 'KiB', 'MiB', 'GiB' ]})
+   *      formatDims([width, height], { units ['m', 'km'] })
+   *
+   * @param {Array.<number>} dims Array of float numbers so that dims.length >= 1
+   * @param {Object} options Used to override default options. Options are:
+   *          mult: exp base between units
+   *          units: array
+   *                  string array specifying the unit names in powers
+   *                  of "options.mult".
+   *          preci: float
+   *                  precision for decimal part (ex: 0.1, 0.01, 0.001 ...)
+   *          joinSep: string
+   *                  separator for numbers serialization (join)
+   *
+   *      Specify options.mult for no-SI units (ex: KiB, MiB use 1024)
+   * @return {string}
+   */
   formatDims: function(dims, options) {
-    // summary: format an array of numbers (called "dimensions")
-    //
-    //      Can typically work on 2D or 3D coordinates, sizes...
-    //      Can also work on simple numbers (call: formatDims([number]))
-    //
-    // dims: array
-    //      Array of float numbers so that dims.length >= 1
-    //
-    // options: object
-    //      Used to override default options. Options are:
-    //          mult: exp base between units
-    //          units: array
-    //                  string array specifying the unit names in powers
-    //                  of "options.mult".
-    //          preci: float
-    //                  precision for decimal part (ex: 0.1, 0.01, 0.001 ...)
-    //          joinSep: string
-    //                  separator for numbers serialization (join)
-    //
-    //      Specify options.mult for no-SI units (ex: KiB, MiB use 1024)
-    //
-    // Examples:
-    //      formatDims([fileWeight], { mult: 1024,
-    //                                 units: ['B', 'KiB', 'MiB', 'GiB' ]})
-    //      formatDims([width, height], { units ['m', 'km'] })
-    //
     var o = dojo.mixin(
       {
         units: [ '', 'K', 'M' ],
