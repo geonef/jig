85361f16be771673cad069fe91beccb7e1d66d6f
Okapi <okapi@geonef.fr>
2011-09-16 00:25:51 +0200

    Carnets: pour le concours....

---
+++ src/geonef/jig/util/color/Gradient.js
@@ -0,0 +1,35 @@
+
+dojo.provide('geonef.jig.util.color.Gradient');
+
+dojo.declare('geonef.jig.util.color.Gradient', null,
+{
+  color1: [0,0,0],
+  color2: [0,0,0],
+
+  /**
+   * @param {Array.<number>} color1
+   * @param {Array.<number>} color2
+   */
+  constructor: function(color1, color2) {
+    this.color1 = color1;
+    this.color2 = color2;
+  },
+
+  /**
+   * @param {number} position between 0 and 1
+   */
+  getColor: function(position) {
+    var ret = [null, null, null];
+    for (var i = 0; i < 3; ++i) {
+      ret[i] = this.color1[i] +
+        (this.color2[i] - this.color1[i]) * position;
+    }
+    return ret;
+  },
+
+  getColorCssHsl: function(position) {
+    var color = this.getColor(position);
+    return 'hsl('+color[0]+','+color[1]+'%,'+color[2]+'%)';
+  }
+
+});
