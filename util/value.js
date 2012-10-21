5aa192d7d06effaf636e67cb7b7d01974bb5dfac
okapi <jf@geonef.fr>
2012-10-21 15:11:41 +0200

    fixed getClass() in util/value and added getModule()

--- util/value.js
+++ util/value.js
@@ -1,35 +1,73 @@
 define([
-         "dojo/_base/lang",
-], function(lang) {
+  "dojo/_base/lang",
+  "dojo/_base/kernel",
+  "dojo/Deferred",
+  // this file uses the global AMD 'require' function
+], function(lang, kernel, Deferred) {
 
-var self = {
+var self = { //--noindent--
 
   /**
-   * Get given class, fetch it if needed
+   * Get given class, fetch it if needed - OBSOLETE!
    *
    * @param {!string|Function} name  the name of the class, or the class itself
    * @param {boolean} require   if false, the class won't be fetched if not available
    * @return {?Function} the class matching 'name', or null of 'require' is true and the class isn't available
    */
   getClass: function(name, requireIt) {
+    kernel.deprecated("geonef/jig/util/value.getClass()",
+                      "Use AMD's require() instead. -- "+name);
+    // console.warn(new Error("geonef/jig/util/value.getClass() is obsolete! "+
+    //                        "Use AMD's require() instead."));
     if (typeof name == 'function') {
       return name;
     }
-    var Class = lang.getObject(name);
+    var Class = require(name);
+    // var Class = lang.getObject(name);
     if (typeof Class != 'function') {
       requireIt = requireIt === undefined || requireIt;
       //console.log('loading class', name, requireIt);
       if (requireIt) {
-        // require(name);
+        var dojoRequire = dojo['require'];
+        if (!dojoRequire) {
+          throw new Error(
+            "geonef/jig/util.getClass('"+name+"') called and dojo.require()"+
+              "not available (probably because dojoConfig.async==false");
+        }
+        // this way of requiring is obsolete:
         dojo['require'](name);
+        Class = lang.getObject(name);
       }
-      Class = lang.getObject(name);
     }
     if (typeof Class != 'function') {
       console.error(name, 'is not a class!', Class, arguments);
       throw new Error(name+' is not a class!');
     }
     return Class;
+  },
+
+  /**
+   * Flexible AMD module getter
+   *
+   * If 'name' is a string, the module is required, using the given
+   * 'contextRequire' function (if provided), or the global one.
+   *
+   * If 'name' is not a string, it is simply returned (through the deferred).
+   *
+   * @param {mixed} name
+   * @param {!Function} contextRequire context-sensitive require function
+   * @return {dojo/Deferred}
+   */
+  getModule: function(name, contextRequire) {
+    var deferred = new Deferred();
+    if (typeof name === "string") {
+      (require || contextRequire)([name], function(module) {
+        deferred.resolve(module);
+      });
+    } else {
+      deferred.resolve(name);
+    }
+    return deferred;
   },
 
 
@@ -81,6 +119,6 @@ var self = {
 
 };
 
-return self;
+  return self;
 
 });
