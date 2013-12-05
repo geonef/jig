8ddd7ab01c557908d63589fac01bd5cff4cdc40c
okapi <jf@geonef.fr>
2013-12-05 15:12:43 +0000

    added data/list/ListTitleMixin

---
+++ data/list/ListTitleMixin.js
@@ -0,0 +1,63 @@
+/**
+ */
+define([
+  "module",
+  "dojo/_base/declare",
+  "dojo/_base/lang",
+  "geonef/jig/button/Action",
+], function(module, declare, lang, Action) {
+
+  return declare(null, {
+
+    paneTitle: "",
+    titleLink: null,
+    enableTitleClick: false,
+
+    /**
+     * Make DOM definitions of pane <h2> - called by concrete class' makeContentNodes()
+     */
+    makePaneTitleNode: function() {
+      var content = this.getPaneTitle();
+      var titleOptions;
+      if (this.titleLink) {
+        titleOptions = {
+          onExecute: this.appView.deferOpen(
+            this.titleLink === true ? this.constructor : this.titleLink,
+            { user: this.user })
+        };
+      } else if (this.enableTitleClick) {
+        titleOptions = {
+          onExecute: lang.hitch(this, this.onTitleClick)
+        };
+      }
+
+      return titleOptions ?
+        [Action, lang.mixin({ nodeName: "h2", label: content, "class": "link" }, titleOptions)]
+      : ['h2', {}, content];
+    },
+
+    /**
+     * Called by makePaneTitleNode() - can be overriden to provide dynamic titles
+     */
+    getPaneTitle: function() {
+      return this.paneTitle;
+    },
+
+    /**
+     * @override for PaneMixin
+     */
+    makeTitle: function(titleNode) {
+      return this.tempDom([
+        ['span', {_insert: titleNode}, this.paneTitle + this.getPagingLabel()]
+      ]);
+    },
+
+    onTitleClick: function() {
+      console.log("onTitleClick", this, arguments);
+    },
+
+    declaredClass: module.id
+
+  });
+
+});
