2b2f67b81653db0efb252b77a1b4291d20bf6ca2
Tony <tony@baudon.xyz>
2018-02-01 14:38:48 +0100

    Remove unnecessary comma

--- input/DateTextBox.js
+++ input/DateTextBox.js
@@ -1,46 +1,45 @@
 define([
-  "module",
-  "dojo/_base/declare",
-  "dijit/form/DateTextBox",
-  "dojo/date/stamp",
+    "module",
+    "dojo/_base/declare",
+    "dijit/form/DateTextBox",
+    "dojo/date/stamp"
 ], function(module, declare, DateTextBox, stamp) {
 
-/**
- * Extension of dijit/DataTextBox to deal with string date value
- *
- * It is used in the same way, except that it will work when set with
- * a string date value.
- *
- * The value getter is unchanged and returns a Date object.
- */
-return declare(DateTextBox,
-{
+    /**
+     * Extension of dijit/DataTextBox to deal with string date value
+     *
+     * It is used in the same way, except that it will work when set with
+     * a string date value.
+     *
+     * The value getter is unchanged and returns a Date object.
+     */
+    return declare(DateTextBox, {
 
-  timestamp: false,
-  autoSerialize: false,
+        timestamp: false,
+        autoSerialize: false,
 
-  _setValueAttr: function(value) {
-    if (typeof value == 'string') {
-      // var displayValue = value.replace(/Z/, '').replace(/-/g, '/');
-      var displayValue = value;
-      // var date = new Date(displayValue);
-      var date = stamp.fromISOString(displayValue);
-      this.set('value', date);
-    } else {
-      this.inherited(arguments);
-    }
-  },
+        _setValueAttr: function(value) {
+            if (typeof value == 'string') {
+                // var displayValue = value.replace(/Z/, '').replace(/-/g, '/');
+                var displayValue = value;
+                // var date = new Date(displayValue);
+                var date = stamp.fromISOString(displayValue);
+                this.set('value', date);
+            } else {
+                this.inherited(arguments);
+            }
+        },
 
-  _getValueAttr: function() {
-    var value = this.inherited(arguments);
-    if (value && this.autoSerialize) {
-      value = this.serialize(value);
-    }
-    return value;
-  },
+        _getValueAttr: function() {
+            var value = this.inherited(arguments);
+            if (value && this.autoSerialize) {
+                value = this.serialize(value);
+            }
+            return value;
+        },
 
-  declaredClass: module.id
+        declaredClass: module.id
 
-});
+    });
 
 });
